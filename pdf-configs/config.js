712a129aca33271735efa30d335ad9b5a837f051
Kenjiro Kuge <kugetyan0211@gmail.com>
2025-12-06 21:54:20 +0900

    fix: Puppeteerの実行時に引数を追加

--- pdf-configs/config.js
+++ pdf-configs/config.js
@@ -13,5 +13,7 @@ module.exports = {
       "footerTemplate": "<section>\n  <div>\n    <span class=\"pageNumber\"></span>\n    / <span class=\"totalPages\"></span>\n  </div>\n</section>"
     },
     stylesheet_encoding: "utf-8",
-    puppeteer_args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    launch_options: {
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    }
   };
